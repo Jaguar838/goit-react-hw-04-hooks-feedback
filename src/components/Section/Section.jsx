4762089932c5d680f721883bee1e89e3d032adf9
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-16 08:17:00 +0300

    Up Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,1 +1,8 @@
-export const Section = () => {};
+import styles from './Section.module.scss';
+import PropTypes from 'prop-types';
+
+export const Section = ({ children }) => <div className={styles.container}>{children}</div>;
+
+Container.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
