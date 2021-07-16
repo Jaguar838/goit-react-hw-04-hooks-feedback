4fcca0cee48a670290d3657ff268203cfd2bf909
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-16 10:47:57 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,12 @@
-import styles from './Section.module.scss';
+import css from './Section.module.scss';
 import PropTypes from 'prop-types';
 
-export const Section = ({ children }) => <div className={styles.section}>{children}</div>;
+export const Section = ({ title, children }) => 
+
+<div className={css.section}>
+<h2 className={css.title}> {title}</h2>  
+{children}
+</div>;
 
 Container.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
