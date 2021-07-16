f938e6dd14413d57f164373aabd9aed3433207eb
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-16 10:50:06 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
       this.setState{(prevState)=>{{[feedback]: prevState[feedback]+1}}}
   }
       
-      render() {
+ render() {
         return (
 <div className={css.App}>
 <Section title='Please leave feedback '>
