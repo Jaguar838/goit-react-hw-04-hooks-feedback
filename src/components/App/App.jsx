0efedfc0e73c81dc1c0f717be240de799fe947df
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-16 10:36:28 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,10 @@ import { FeedbackOptions } from 'components/FeedbackOptions';
 import { Section } from 'components/Section';
 import { Statistics } from 'components/Statistics';
 import { Notification } from 'components/Notification';
-import './App.css';
-import {} from 'server/constants';
+
+import {FEEDBACK_OPTIONS} from 'server/constants';
+
+import css from 'components/App.module.css';
 
 class App extends Component {
   state = {
@@ -11,38 +13,19 @@ class App extends Component {
     neutral: 0,
     bad: 0,
   }
+  
   handleFeedback = ({ target }) => {
     const { feedback } = <target className="dataset">
       this.setState{(prevState)=>{{[feedback]: prevState[feedback]+1}}}
   }
       
       render() {
-      
         return (
-        <div>
-        <Section title='Please leave feedback '>
-          <FeedbackOptions options={FEEDBACK_OPTIONS} onLeaveFeedback/>
-        </Section>
-        </div>)
-      }
-        }
-
-div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className={css.App}>
+<Section title='Please leave feedback '>
+<FeedbackOptions options={FEEDBACK_OPTIONS} onLeaveFeedback/>
+</Section>
+</div>
+  )}
 }
-
 export default App;
